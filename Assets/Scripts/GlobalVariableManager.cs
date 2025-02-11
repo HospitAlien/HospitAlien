96da9bcc81dbf2b314dbe7f84a209e0c8a8bad4e
Sean Xu <tk22444@bristol.ac.uk>
2025-02-11 16:13:48 +0000

    add a manager and test scene

---
+++ Assets/Scripts/GlobalVariableManager.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using System.IO;
+
+// This script is used to manage global variables that need to be shared between objects and scene.
+// It also manages the saving and loading for variable that need to be stored.
+// To use this in other object in other scene, use the following code:
+// GlobalVariableManager gvm = FindFirstObjectByType<GlobalVariableManager>();
+// Note: This script must be attached ONCE ONLY to a GameObject in the start scene!
+public class GlobalVariableManager : MonoBehaviour
+{
+    public GameSettingsIO gameSettings;
+    public LeaderBoardIO leaderBoard;
+
+    public bool GameStarted;
+
+    // Place to save the file
+    private string settingFilePath;
+    private string leaderBoardFilePath;
+
+    private void Awake()
+    {
+        // Get the file path for the settings file
+        settingFilePath = Path.Combine(Application.persistentDataPath, "settings.json");
+        leaderBoardFilePath = Path.Combine(Application.persistentDataPath, "LeaderBoard.json");
+        GameStarted = false;
+
+        // Load the settings from the file
+        LoadSettings();
+        LoadLeaderBoard();
+
+        // Make sure this object is not destroyed when loading a new scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Load the settings if the file exists
+    public void LoadSettings()
+    {
+        if (File.Exists(settingFilePath))
+        {
+            string json = File.ReadAllText(settingFilePath);
+            JsonUtility.FromJsonOverwrite(json, gameSettings);
+        }
+        else
+        {
+            Debug.Log("Setting file not found, using default settings.");
+        }
+    }
+
+
+    // Save the settings to a file
+    public void SaveSettings()
+    {
+        string json = JsonUtility.ToJson(gameSettings, prettyPrint: true);
+        File.WriteAllText(settingFilePath, json);
+        Debug.Log("Game setting saved to:" + settingFilePath);
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveSettings();
+    }
+
+    // Load the leaderboard if the file exists
+    public void LoadLeaderBoard()
+    {
+        if (File.Exists(leaderBoardFilePath))
+        {
+            string json = File.ReadAllText(leaderBoardFilePath);
+            JsonUtility.FromJsonOverwrite(json, leaderBoard);
+        }
+        else
+        {
+            Debug.Log("LeaderBoard file not found, using default settings.");
+        }
+    }
+
+    // Save the leaderboard to a file
+    public void SaveLeaderBoard()
+    {
+        string json = JsonUtility.ToJson(leaderBoard, prettyPrint: true);
+        File.WriteAllText(leaderBoardFilePath, json);
+        Debug.Log("LeaderBoard saved to:" + leaderBoardFilePath);
+    }
+}
