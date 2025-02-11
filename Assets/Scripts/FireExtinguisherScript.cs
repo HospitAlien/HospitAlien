5c3303d728b66e1f147351bf1b35ce4faac88db9
Hugh <bz22499@bristol.ac.uk>
2025-02-11 17:09:55 +0000

    fire script

--- Assets/Scripts/FireExtinguisherScript.cs
+++ Assets/Scripts/FireExtinguisherScript.cs
@@ -1,43 +1,39 @@
 using UnityEngine;
-using UnityEngine.XR;
+using System.Linq; // Needed for .Any() or .FirstOrDefault()
+using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
 
-
-public class FireExtinguisherTriggerDebug : MonoBehaviour
+public class FireExtinguisher : MonoBehaviour
 {
-    private InputDevice leftHandDevice;
-    private InputDevice rightHandDevice;
+    // Reference to the DistanceHandGrabInteractable on the fire extinguisher.
+    // Assign this via the Inspector or let the script auto–find it.
+    [SerializeField]
+    private DistanceHandGrabInteractable _distanceGrabInteractable;
 
-    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabInteractable; 
-
-    private void Start()
+    private void Awake()
     {
-        // Get the VR controller devices for both hands
-        leftHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
-        rightHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
-
-        // Get the XRGrabInteractable component to check if the fire extinguisher is being held
-        grabInteractable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+        // If not assigned in the Inspector, try to find the interactable in our children.
+        if (_distanceGrabInteractable == null)
+        {
+            _distanceGrabInteractable = GetComponentInChildren<DistanceHandGrabInteractable>();
+            if (_distanceGrabInteractable == null)
+            {
+                Debug.LogError("DistanceHandGrabInteractable not found on children of " + gameObject.name);
+            }
+        }
     }
 
     private void Update()
     {
-        // Check if the fire extinguisher is being grabbed
-        if (grabInteractable.isSelected)
+        // Check if the extinguisher is currently grabbed.
+        // We do this by checking if the collection of selecting interactors is non–empty.
+        if (_distanceGrabInteractable != null && _distanceGrabInteractable.SelectingInteractors.Any())
         {
-            // Check if either hand is holding the fire extinguisher and the trigger button is pressed
-            CheckForTriggerPress(leftHandDevice, "Left Hand Trigger Pressed!");
-            CheckForTriggerPress(rightHandDevice, "Right Hand Trigger Pressed!");
-        }
-    }
-
-    private void CheckForTriggerPress(InputDevice device, string logMessage)
-    {
-        if (device.isValid)
-        {
-            bool triggerPressed = false;
-            if (device.TryGetFeatureValue(CommonUsages.triggerButton, out triggerPressed) && triggerPressed)
+            // Check for a trigger press on either controller.
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch) ||
+                OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
             {
-                Debug.Log(logMessage);  // Log the message when trigger is pressed
+                Debug.Log("Fire extinguisher trigger pressed while held!");
             }
         }
     }
