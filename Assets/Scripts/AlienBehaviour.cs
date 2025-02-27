933f13083eb498268eb206b1ce6fa225f6af683d
Veeraj270 <ro22780@bristol.ac.uk>
2025-02-27 12:56:02 +0000

    Added translator object to hold into alien's when talking rather than  putting controller in

--- Assets/Scripts/AlienBehaviour.cs
+++ Assets/Scripts/AlienBehaviour.cs
@@ -172,7 +172,7 @@ public class AlienBehaviour : MonoBehaviour
     // Detect collision with the player's controller
     void OnTriggerEnter(Collider collider)
     {
-        if (collider.CompareTag("Controller")) // Cure the alien if it collides with the controller
+        if (collider.CompareTag("Translator")) // Cure the alien if it collides with the controller
         {
             //Voice chat function
             alienVoice.ActivateListening();
