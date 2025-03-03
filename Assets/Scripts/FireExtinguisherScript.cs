1c7b0e4fd782be12c6da8b089886012cc351b301
Sean Xu <tk22444@bristol.ac.uk>
2025-03-03 17:36:42 +0000

    add grab and use function

--- Assets/Scripts/FireExtinguisherScript.cs
+++ Assets/Scripts/FireExtinguisherScript.cs
@@ -1,20 +1,36 @@
+using Oculus.Interaction.HandGrab;
 using UnityEngine;
 
-public class FireExtinguisherTrigger : MonoBehaviour
+public class FireExtinguisherTrigger : MonoBehaviour, IHandGrabUseDelegate
 {
     private float pressThreshold = 0.5f;
 
-
-    private void Update()
+    public void BeginUse()
     {
-        float leftTrigger = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
-        float rightTrigger = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
-
-        bool isPressed = leftTrigger >= pressThreshold || rightTrigger >= pressThreshold;
-
-        if (isPressed)
-        {
-            Debug.Log("Trigger is pressed!");
-        }
+        Debug.Log("Begin use");
     }
+
+    public float ComputeUseStrength(float strength)
+    {
+        Debug.Log("Compute use strength, strength: " + strength);
+        return strength;
+    }
+
+    public void EndUse()
+    {
+        Debug.Log("End use");
+    }
+
+    // private void Update()
+    // {
+    //     float leftTrigger = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
+    //     float rightTrigger = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+
+    //     bool isPressed = leftTrigger >= pressThreshold || rightTrigger >= pressThreshold;
+
+    //     if (isPressed)
+    //     {
+    //         Debug.Log("Trigger is pressed!");
+    //     }
+    // }
 }
