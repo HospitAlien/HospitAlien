533ad71d23d876a8337783259e1ff9082de6d77a
Luke Trott <123554537+zq22843@users.noreply.github.com>
2025-03-04 19:01:29 +0000

    I THINK WORKING CANT TEST

--- Assets/Scripts/ShrapnelBehaviour.cs
+++ Assets/Scripts/ShrapnelBehaviour.cs
@@ -5,11 +5,12 @@ using System.Collections;
 
 public class ShrapnelBehaviour : MonoBehaviour
 {
-
     private bool insideAlien;
     public GrabInteractable GrabInteractable;
     private Coroutine destroyCoroutine;
+
     private Rigidbody rb;
+    private Collider collider;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,10 +19,30 @@ public class ShrapnelBehaviour : MonoBehaviour
         GrabInteractable.WhenSelectingInteractorAdded.Action += ObjectHeld;
         GrabInteractable.WhenSelectingInteractorRemoved.Action += ObjectReleased;
         rb = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+
+
+        Collider parentCollider = GetComponent<Collider>();
+
+        // Get all colliders in the children of the current object
+        Collider[] childrenColliders = GetComponentsInChildren<Collider>();
+
+        foreach (Collider col in childrenColliders)
+        {
+            // Check if the collider belongs to the parent (since it's a child, skip parent collider itself)
+            if (col != parentCollider)
+            {
+                // Ignore collision between the child's collider and the parent's collider
+                Physics.IgnoreCollision(col, parentCollider);
+            }
+        }
     }
 
     private void ObjectHeld(GrabInteractor interactor)
     {
+        //when the object is held i want it to be isTrigger
+        collider.isTrigger = true;
+
         if (destroyCoroutine != null)
         {
             StopCoroutine(destroyCoroutine);
@@ -31,6 +52,8 @@ public class ShrapnelBehaviour : MonoBehaviour
 
     private void ObjectReleased(GrabInteractor interactor)
     {
+        collider.isTrigger = false;
+
         if (!insideAlien)
         {
             destroyCoroutine = StartCoroutine(DestroyObjectAfterTime(5f));
@@ -75,8 +98,9 @@ public class ShrapnelBehaviour : MonoBehaviour
                     StopCoroutine(destroyCoroutine);
                     destroyCoroutine = null;
                     Debug.Log("Destroy coroutine stopped.");
+
+                    rb.isKinematic = true;
                 }
-                rb.isKinematic = true;
 
             }
 
