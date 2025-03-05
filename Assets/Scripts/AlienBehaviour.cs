ad4519b4fc51134a0b520cca17493541a9cbc0f9
Luke Trott <123554537+zq22843@users.noreply.github.com>
2025-03-05 13:30:59 +0000

    changed the look of the shrapnel

--- Assets/Scripts/AlienBehaviour.cs
+++ Assets/Scripts/AlienBehaviour.cs
@@ -199,11 +199,20 @@ public class AlienBehaviour : MonoBehaviour
             // Use Unity's Random class for generating random numbers
             float offsetX = UnityEngine.Random.Range(-0.3f, 0.3f);
             float offsetY = UnityEngine.Random.Range(0f, 0.5f);
-            float offsetZ = 0f;  // Depth offset (use if you want to spawn swords further into the body)
+            float offsetZ = 0.1f;  // Depth offset (use if you want to spawn swords further into the body)
 
             // Spawn the new shrapnel at the calculated position
             Vector3 newPosition = bodyTransform.position + new Vector3(offsetX, offsetY, offsetZ);
-            GameObject newShrapnel = Instantiate(shrapnel, newPosition, bodyTransform.rotation);
+
+            float randomXRotation = UnityEngine.Random.Range(-30f, 30f);
+            float randomZRotation = UnityEngine.Random.Range(0f, 360f);
+            
+            // Construct a new rotation with random x and z rotation values, keeping the y rotation the same as bodyTransform
+            Quaternion randomRotation = Quaternion.Euler(randomXRotation, bodyTransform.rotation.eulerAngles.y, randomZRotation);
+
+
+
+            GameObject newShrapnel = Instantiate(shrapnel, newPosition, randomRotation);
             newShrapnel.transform.SetParent(transform); // Set the shrapnel as a child of the player
 
             count++;
