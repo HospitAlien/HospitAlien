f1d8d858d7426596f251f08e8ea8bb3971597b8e
Veeraj270 <ro22780@bristol.ac.uk>
2025-03-07 19:05:05 +0000

    Creates a collider to detect axe on spawn

--- Assets/Scripts/AmputationBehaviour.cs
+++ Assets/Scripts/AmputationBehaviour.cs
@@ -5,7 +5,7 @@ public class AmputationBehaviour : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CreateCollider();
     }
 
     // Update is called once per frame
@@ -13,4 +13,13 @@ public class AmputationBehaviour : MonoBehaviour
     {
         
     }
+
+    public void CreateCollider()
+    {
+        // Create the first BoxCollider with a specific center
+        BoxCollider collider = gameObject.AddComponent<BoxCollider>();
+        collider.center = new Vector3(-0.38f, 1.23f, -0.01f);
+        collider.size = new Vector3(0.13f, 0.2f, 0.23f);
+    }
+
 }
