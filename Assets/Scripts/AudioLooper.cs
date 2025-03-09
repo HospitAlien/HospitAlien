5a48c509594ab30dfe9190c4c411be24d5d2ad75
Luke Trott <zq22843@bristol.ac.uk>
2025-03-09 15:51:05 +0000

    added audio to extinguisher

---
+++ Assets/Scripts/AudioLooper.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioLooper : MonoBehaviour
+{
+    [SerializeField] private double loopStartTime;
+    [SerializeField] private double loopEndTime;
+
+    private int loopStartSamples;
+    private int loopEndSamples;
+    private int loopLengthSamples;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        loopStartSamples = (int)(loopStartTime * audioSource.clip.frequency);
+        loopEndSamples = (int)(loopEndTime * audioSource.clip.frequency);
+        loopLengthSamples = loopEndSamples - loopStartSamples;
+    }
+
+    private void Update()
+    {
+        if (audioSource.timeSamples >= loopEndSamples) { audioSource.timeSamples -= loopLengthSamples; }
+    }
+}
+
+//refernece https://www.reddit.com/r/Unity3D/comments/m4nq61/quickandeasy_audio_looping_component/?rdt=40841
