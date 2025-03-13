3e87cc098d2abb2c466c90416299c2ae52b8d1ad
Luke Trott <123554537+zq22843@users.noreply.github.com>
2025-03-13 19:23:16 +0000

    added model

--- Assets/Scripts/AlienBehaviour.cs
+++ Assets/Scripts/AlienBehaviour.cs
@@ -113,37 +113,37 @@ public class AlienBehaviour : MonoBehaviour
     {
         System.Random random = new System.Random();
 
-        // if (random.NextDouble() < 0.4)
-        // {
-        //     status.needsInjection = true;
-        //     sweatParticles.Play();
-        //     reward += 100;
-        // }
-        // if (random.NextDouble() < 0.4)
-        // {
-        //     status.needsExtinguishing = true;
-        //     fireParticles.Play();
-        //     reward += 100;
-        // }
+        if (random.NextDouble() < 0.4)
+        {
+            status.needsInjection = true;
+            sweatParticles.Play();
+            reward += 100;
+        }
+        if (random.NextDouble() < 0.4)
+        {
+            status.needsExtinguishing = true;
+            fireParticles.Play();
+            reward += 100;
+        }
 
-        // if (random.NextDouble() < 0.4)
-        // {
-        //     status.shrapnelCount = 4;
-        //     reward += 100;
-        //     status.hasShrapnel = true;
-        //     initiateShrapnel();
-        // }
+        if (random.NextDouble() < 0.4)
+        {
+            status.shrapnelCount = 4;
+            reward += 100;
+            status.hasShrapnel = true;
+            initiateShrapnel();
+        }
 
-        // if(random.NextDouble() < 0.4){
+        if(random.NextDouble() < 0.4){
 
-        //     if(random.NextDouble() < 0.5){ //shrink
-        //         status.curSize = -1;
-        //         transform.localScale /= 2f;
-        //     }else{ //enlargement
-        //         status.curSize = 1;
-        //         transform.localScale *= 1.4f;
-        //     }
-        // }
+            if(random.NextDouble() < 0.5){ //shrink
+                status.curSize = -1;
+                transform.localScale /= 2f;
+            }else{ //enlargement
+                status.curSize = 1;
+                transform.localScale *= 1.4f;
+            }
+        }
 
         if(true){
             status.numberOfBadEyes = 1;
