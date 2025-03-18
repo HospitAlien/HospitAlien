ef4695b644e1773a89a2a7ef70a80f2b0ce102f2
Veeraj270 <ro22780@bristol.ac.uk>
2025-03-18 17:20:21 +0000

    Script creates multiple colliders for multiple limbs

--- Assets/Scripts/AmputationBehaviour.cs
+++ Assets/Scripts/AmputationBehaviour.cs
@@ -55,10 +55,53 @@ public class AmputationBehaviour : MonoBehaviour
 
     public void CreateCollider()
     {
-        // Create the first BoxCollider with a specific center
-        BoxCollider collider = gameObject.AddComponent<BoxCollider>();
-        collider.center = new Vector3(-0.62f, 0.81f, -0.04f);
-        collider.size = new Vector3(0.22f, 0.08f, 0.23f);
+        for (int i = 0; i < 4; i++)
+        {
+            if (alienBehaviour.amputations.limbs[i])
+            {
+                GameObject colliderObject = null;
+                Vector3 offset = Vector3.zero;
+                Vector3 size = Vector3.zero;
+
+                switch (i)
+                {
+                    case 0:
+                        colliderObject = new GameObject("LeftHand");
+                        offset = new Vector3(-0.62f, 0.81f, -0.04f);
+                        size = new Vector3(0.22f, 0.08f, 0.23f);
+                        break;
+                    case 1:
+                        colliderObject = new GameObject("RightHand");
+                        offset = new Vector3(0.62f, 0.81f, -0.04f);
+                        size = new Vector3(0.22f, 0.08f, 0.23f);
+                        break;
+                    case 2:
+                        colliderObject = new GameObject("LeftLeg");
+                        offset = new Vector3(-0.27f, 0.37f, -0.01f);
+                        size = new Vector3(0.25f, 0.08f, 0.26f);
+                        break;
+                    case 3:
+                        colliderObject = new GameObject("RightLeg");
+                        offset = new Vector3(0.27f, 0.37f, -0.01f);
+                        size = new Vector3(0.25f, 0.08f, 0.26f);
+                        break;
+                }
+
+                if (colliderObject != null)
+                {
+                    // Make the new GameObject a child of the current GameObject.
+                    colliderObject.transform.parent = transform;
+                    // Set the local position to the offset originally used for the collider center.
+                    colliderObject.transform.localPosition = offset;
+
+                    // Add the BoxCollider component. Since the object is positioned correctly,
+                    // we can set its center to zero.
+                    BoxCollider boxCollider = colliderObject.AddComponent<BoxCollider>();
+                    boxCollider.center = Vector3.zero;
+                    boxCollider.size = size;
+                }
+            }
+        }
     }
 
     public void CreateBloodParticles()
