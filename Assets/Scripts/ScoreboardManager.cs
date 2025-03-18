3c67115e9aad22c43b92a1cce73271ac464589fe
Luke Trott <123554537+zq22843@users.noreply.github.com>
2025-03-18 16:11:15 +0000

    prefab now has accsess to the script

--- Assets/Scripts/ScoreboardManager.cs
+++ Assets/Scripts/ScoreboardManager.cs
@@ -7,28 +7,58 @@ public class ScoreBoardManager : MonoBehaviour
 
     public Scrollbar scoreScrollbar;  
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI currentRank;
 
-    public int previousThreshold = 0;
+    private int[] targetScores;
+    private string[] targetRanks;
+    private int currentThreshhold;
+    
     public int currentScore = 0;
-    public int targetScore = 100;
-
+    
+    void Start()
+    {
+        targetScores = new int[] {0,2500,5000,7500,10000,12500,17500};
+        targetRanks = new string[]
+        {
+            "Medical Student", 
+            "Resident", 
+            "Fellow", 
+            "Attending Physician", 
+            "Department Head", 
+            "Medical Director", 
+            "Consultant Doctor"
+        };
+        currentThreshhold = 0;
+        currentRank.text = targetRanks[currentThreshhold];
+    }
 
     void UpdateScrollbarAndText()
     {
-        if (targetScore > 0)
-        {
-            float progess = currentScore - previousThreshold;
-            float totalGap = targetScore - previousThreshold;
-
-
+        if(currentThreshhold != targetRanks.Length -1){
+            float progess = currentScore - targetScores[currentThreshhold];
+            float totalGap = targetScores[currentThreshhold+1] - targetScores[currentThreshhold];
             float scrollbarValue = progess / totalGap;
             scoreScrollbar.size = Mathf.Clamp01(scrollbarValue);
+        }else{
+            scoreScrollbar.size = 1;
         }
         scoreText.text = $"Score: {currentScore}";
     }
 
     void Update(){
-        Debug.Log(" GOT HERE");
+        if(currentThreshhold<targetScores.Length-1)
+        {   
+            if(targetScores[currentThreshhold+1] <= currentScore){ //if we have reached the next level
+                currentThreshhold++;
+                currentRank.text = targetRanks[currentThreshhold];
+            }
+        }
+        if(currentThreshhold != 0){
+            if(targetScores[currentThreshhold] > currentScore){ //if we have reached the next level
+                currentThreshhold--;
+                currentRank.text = targetRanks[currentThreshhold];
+            }
+        }
         UpdateScrollbarAndText();
     }
 
@@ -36,9 +66,8 @@ public class ScoreBoardManager : MonoBehaviour
     public void IncrementScore(int incrementAmount)
     {
         currentScore += incrementAmount;
-        if (currentScore > targetScore)
-        {
-            currentScore = targetScore;
+        if(targetScores[currentThreshhold+1] < currentScore){ //if we have reached the next level
+            currentThreshhold++;
         }
         UpdateScrollbarAndText();
     }
