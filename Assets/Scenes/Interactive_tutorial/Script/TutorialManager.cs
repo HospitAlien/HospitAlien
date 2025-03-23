ffbf1b2c5c7b266fb1597bf78e8793731c1e0a5d
Sean Xu <tk22444@bristol.ac.uk>
2025-03-23 16:35:10 +0000

    finish select tutorial

--- Assets/Scenes/Interactive_tutorial/Script/TutorialManager.cs
+++ Assets/Scenes/Interactive_tutorial/Script/TutorialManager.cs
@@ -8,6 +8,7 @@ public class TutorialManager : MonoBehaviour
     public MoveManager moveManager;
     public MenuManager menuManager;
     private bool isInitd = false;
+    public GameObject SelectTutorial;
 
     void Start()
     {
@@ -15,6 +16,7 @@ public class TutorialManager : MonoBehaviour
         mainCamera = Camera.main;
         mainCamera.cullingMask = LayerMask.GetMask("InitUI");
         moveManager.DisableMovement();
+        SelectTutorial.SetActive(false);
     }
 
     void Update()
@@ -27,21 +29,26 @@ public class TutorialManager : MonoBehaviour
         }
     }
 
-    private void InitCamera()
-    {
-        FindFirstObjectByType<CameraHeightManager>().ResetCameraHeight();
-        mainCamera.cullingMask = -1;
-        isInitd = true;
-        InitText.SetActive(false);
-    }
-
-
-    public void MoveToPlayer(GameObject obj, Vector3 offset = default)
+    private void MoveToPlayer(GameObject obj, Vector3 offset = default)
     {
         Vector3 targetPosition = mainCamera.transform.position + mainCamera.transform.forward * 1f;
         targetPosition += offset;
         obj.transform.position = targetPosition;
         Vector3 directionToCamera = mainCamera.transform.position - obj.transform.position;
         obj.transform.rotation = Quaternion.LookRotation(-directionToCamera);
+    }
+
+    private void InitCamera()
+    {
+        FindFirstObjectByType<CameraHeightManager>().ResetCameraHeight();
+        mainCamera.cullingMask = -1;
+        isInitd = true;
+        InitText.SetActive(false);
+        SelectTutorial.SetActive(true);
+    }
+
+    public void FinishSelectTutorial()
+    {
+        SelectTutorial.SetActive(false);
     }
 }
