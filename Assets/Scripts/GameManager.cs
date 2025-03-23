944278dd9903e4b49f7955bf589bd2c1c99d063b
Veeraj270 <123737105+Veeraj270@users.noreply.github.com>
2025-03-23 22:11:12 +0000

    Lots of bugs: Need to redo arm attachment coords it seems and fix arm grab prefab. Then look into whether we need separate left right prefabs.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -236,7 +236,9 @@ public class GameManager : MonoBehaviour
         Transform spawnPoint = spawnLocations[newSpot];
         GameObject patient;
         int alienType = Random.Range(1, 3);
-        if(alienType == 1)
+        alienType = 2;
+
+        if (alienType == 1)
         {
             patient = Instantiate(purpleAlienPrefab, Portal.transform.position + new Vector3(1, 0, 0), Quaternion.identity);
         }
