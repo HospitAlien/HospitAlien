0cc39531c32787fa3c9ff8bc2b57f95c8253bc1d
Hugh Kiggell <123553220+bz22499@users.noreply.github.com>
2025-03-31 16:37:28 +0100

    Orange alien added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviour
 
     public GameObject purpleAlienPrefab;
     public GameObject greenAlienPrefab;
+    public GameObject orangeAlienPrefab;
 
     public GhostEvent ghostEvent;
     public GameObject pizzaPrefab;
@@ -344,14 +345,18 @@ public class GameManager : MonoBehaviour
         // Select a random spawn location from the spawnLocations array
         Transform spawnPoint = spawnLocations[newSpot];
         GameObject patient;
-        int alienType = Random.Range(1, 3);
+        int alienType = Random.Range(1, 4);
         if (alienType == 1)
         {
             patient = Instantiate(purpleAlienPrefab, Portal.transform.position + new Vector3(1, 0, 0), Quaternion.identity);
         }
-        else
+        else if (alienType == 2)
         {
             patient = Instantiate(greenAlienPrefab, Portal.transform.position + new Vector3(1, 0, 0), Quaternion.identity);
+        }
+        else
+        {
+            patient = Instantiate(orangeAlienPrefab, Portal.transform.position + new Vector3(1, 0, 0), Quaternion.identity);
         }
 
         // Access the AlienBehaviour (or equivalent) script on the newly spawned patient and set its target
--- Assets/Scripts/Status.cs
+++ Assets/Scripts/Status.cs
@@ -226,6 +226,64 @@ public class Status
         }
     }
 
+    public void initiateStatus(OrangeAlien alien, int gameStage){
+        System.Random random = new System.Random();
+
+        int numberOfIllnesses;
+        if(gameStage == 0){ //first stage
+            numberOfIllnesses = random.Next(1, 3);
+        }else if(gameStage == 1){
+            numberOfIllnesses = random.Next(2,5);
+        }else{
+            numberOfIllnesses = random.Next(3,5);
+        }
+
+
+        List<Action> illnesses = new List<Action>();
+        illnesses.Add(() => {
+            needsInjection = true;
+            alien.sweatParticles.Play();
+            alien.reward += 100;
+        });
+
+        illnesses.Add(() => {
+            needsExtinguishing = true;
+            alien.fireParticles.Play();
+            alien.reward += 50;
+        });
+
+        illnesses.Add(() => {
+            shrapnelCount = 4;
+            alien.reward += 150;
+            hasShrapnel = true;
+            alien.initiateShrapnel();
+        });
+
+        illnesses.Add(() => {
+            needsAnAmputation = true;
+            needsAmputation = new bool[] { false, false, false, false };
+            needsAttatchment = new bool[] { false, false, false, false }; //initially none of them need attatching it is only once a limb is removed
+            alien.initiateAmputation();
+        });
+
+        illnesses.Add(() => {
+            alien.reward += 100;
+            if (random.NextDouble() < 0.5) { // shrink
+                curSize = -1;
+                alien.transform.localScale /= 2f;
+            }
+            else { // enlargement
+                curSize = 1;
+                alien.transform.localScale *= 1.4f;
+            }
+        });
+
+        List<Action> shuffledIllnesses = illnesses.OrderBy(x => random.Next()).ToList();
+        for (int i = 0; i < numberOfIllnesses; i++) {
+            shuffledIllnesses[i]();
+        }
+    }
+
     public void amputate(int limb){
         needsAmputation[limb] = false;
         needsAttatchment[limb] = true;
